3c70495fe2d6335eb1e8d8bcf8fb40c184895517
vils007 <vils007@mail.ru>
2017-06-22 14:16:50 +0400

    Date

--- src/main/java/date/Main.java
+++ src/main/java/date/Main.java
@@ -1,13 +1,16 @@
 package date;
 
 import java.text.*;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        String str = "20170622:103033";
+        String str = "2017/06/22 10:30:33";
 
-        DateFormat dataFormat = new SimpleDateFormat("yyyyMMdd:HHmmss");
+        DateFormat dataFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
         System.out.println(dataFormat.parse(str));
+        System.out.println(dataFormat.format(new Date()));
+        System.out.println(dataFormat.format(System.currentTimeMillis()));
     }
 }
